81377fa90e28760c7de833c7b692ccad3faa86bb
yibei333 <Developer333@hotmail.com>
2024-04-30 18:33:04 +0800

    token

--- pack/token.bat
+++ pack/token.bat
@@ -1,6 +1,6 @@
 @echo off
 
 curl -X POST --data-urlencode "grant_type=password" --data-urlencode "username=%email%" --data-urlencode "password=%password%" --data-urlencode "client_id=%clientid%" --data-urlencode "client_secret=%clientsecret%" --data-urlencode "scope=projects user_info issues notes" https://gitee.com/oauth/token >> token.txt
-echo "token=$(cat ./pack/token.txt)" >> $ENV:GITHUB_OUTPUT
+echo "token=$(cat token.txt)" >> $ENV:GITHUB_OUTPUT
 
 pause
