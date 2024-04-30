7ea509f8e77d0722e2af55014d4ac3bc7fa76f89
yibei333 <Developer333@hotmail.com>
2024-04-30 18:22:56 +0800

    token

--- pack/sync.bat
+++ pack/sync.bat
@@ -1,8 +1,5 @@
 @echo off
 
-echo "clientid from env:%client-id%"
-echo "clientsecret from env:%client-secret%"
-echo "email from env:%email%"
-echo "password from env:%password%"
+echo "token from env:%token%"
 
 pause
