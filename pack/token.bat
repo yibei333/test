ca4eebe0cf9cc1109928c9484e44624849599b66
developer333 <Developer333@hotmail.com>
2024-04-30 20:21:48 +0800

    aa

--- pack/token.bat
+++ pack/token.bat
@@ -3,7 +3,7 @@
 curl -X POST --data-urlencode "grant_type=password" --data-urlencode "username=%email%" --data-urlencode "password=%password%" --data-urlencode "client_id=%clientid%" --data-urlencode "client_secret=%clientsecret%" --data-urlencode "scope=projects user_info issues notes" https://gitee.com/oauth/token >> token.txt
 
 set /p temp=<"token.txt"
-echo %email%
+echo "username=%email%"
 echo 123
 echo %temp%
 echo 234
